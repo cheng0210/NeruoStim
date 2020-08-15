d2d5faeddd9d2d0a8fdd4a79305482ab37ae9512
Jacob <41574808+cheng0210@users.noreply.github.com>
2020-08-15 23:07:23 +1000

    Revert "add spi"
    This reverts commit 2f812e5c877e90fd1b9cfc11ec77b010cde83750.

--- NS-STM32/Core/Src/stm32wbxx_hal_msp.c
+++ NS-STM32/Core/Src/stm32wbxx_hal_msp.c
@@ -149,10 +149,9 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
     /**SPI1 GPIO Configuration
     PA1     ------> SPI1_SCK
     PA4     ------> SPI1_NSS
-    PA6     ------> SPI1_MISO
     PA7     ------> SPI1_MOSI
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_4|GPIO_PIN_6|GPIO_PIN_7;
+    GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_4|GPIO_PIN_7;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -185,10 +184,9 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi)
     /**SPI1 GPIO Configuration
     PA1     ------> SPI1_SCK
     PA4     ------> SPI1_NSS
-    PA6     ------> SPI1_MISO
     PA7     ------> SPI1_MOSI
     */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_1|GPIO_PIN_4|GPIO_PIN_6|GPIO_PIN_7);
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_1|GPIO_PIN_4|GPIO_PIN_7);
 
   /* USER CODE BEGIN SPI1_MspDeInit 1 */
 
