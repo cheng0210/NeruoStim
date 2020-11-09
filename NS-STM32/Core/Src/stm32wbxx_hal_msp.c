2ebab2432b7a74ceb68c85136cce459488a8d208
Jacob <41574808+cheng0210@users.noreply.github.com>
2020-11-09 13:52:10 +1100

    fix interrupt priority

--- NS-STM32/Core/Src/stm32wbxx_hal_msp.c
+++ NS-STM32/Core/Src/stm32wbxx_hal_msp.c
@@ -134,7 +134,7 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
     __HAL_LINKDMA(hadc,DMA_Handle,hdma_adc1);
 
     /* ADC1 interrupt Init */
-    HAL_NVIC_SetPriority(ADC1_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(ADC1_IRQn, 2, 0);
     HAL_NVIC_EnableIRQ(ADC1_IRQn);
   /* USER CODE BEGIN ADC1_MspInit 1 */
 
